e477ef0f2e43251363bb3f7839a9c7c8c9e5a669
Brandon Seydel <brandonseydel@hotmail.com>
2021-02-25 17:45:08 -0600

    push latest listeners for v1/2

--- src/detector/detector.ts
+++ src/detector/detector.ts
@@ -1,17 +1,11 @@
 
+// detects V1
+window.addEventListener('au-composed', () => {
+  chrome.runtime.sendMessage({ aureliaDetected: true, version: 1 });
+}, { once: true });
 
-document.addEventListener("DOMContentLoaded", function (event) {
-  if (window.document.querySelectorAll('[aurelia-app]').length > 0) {
-    chrome.runtime.sendMessage({ aureliaDetected: true, version: 1 });
-    return;
-  }
 
-  const all = document.querySelectorAll('*')
-  for (let i = 0; i < all.length; i++) {
-    const aurelia = (all[i] as any).$aurelia;
-    if (aurelia) {
-      chrome.runtime.sendMessage({ aureliaDetected: true, version: 2 });
-      return;
-    }
-  }
-});
+// detects V2
+window.addEventListener('au-started', (test) => {
+  chrome.runtime.sendMessage({ aureliaDetected: true, version: 2 });
+}, { once: true });
